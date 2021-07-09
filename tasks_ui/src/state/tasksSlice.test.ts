b3f672dbc9788a784439b99f492ed05eb7b7e311
Kenichiro Yasuda <kenichiro.yasuda@mail.rakuten.com>
2021-07-09 13:50:32 +0900

    Step7: add tasks slice

---
+++ tasks_ui/src/state/tasksSlice.test.ts
@@ -0,0 +1,304 @@
+import { tasksSlice, index, create, get, update, destroy } from './tasksSlice'
+const fetchMock = require('fetch-mock-jest')
+
+const reducer = tasksSlice.reducer
+const actions = tasksSlice.actions
+
+const initialState = {
+  tasks: [],
+  pending: false,
+}
+
+describe('tasks slice', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
+  describe('edit actions', () => {
+    const task = {
+      id: 1,
+      name: 'task1',
+      description: 'description1',
+      edit: { name: false, description: false },
+    }
+
+    describe('showName', () => {
+      it('should set edit.name as true', () => {
+        const action = actions.showName(task.id)
+        const actual = reducer(
+          { tasks: [{ ...task, edit: { name: true, description: false } }] },
+          action
+        )
+
+        expect(actual.tasks[0].edit.name).toEqual(false)
+      })
+    })
+    describe('editName', () => {
+      it('should set edit.name as false', () => {
+        const action = actions.editName(task.id)
+        const actual = reducer({ tasks: [task] }, action)
+
+        expect(actual.tasks[0].edit.name).toEqual(true)
+      })
+    })
+    describe('showDescription', () => {
+      it('should set edit.description as true', () => {
+        const action = actions.showDescription(task.id)
+        const actual = reducer(
+          { tasks: [{ ...task, edit: { name: false, description: true } }] },
+          action
+        )
+
+        expect(actual.tasks[0].edit.description).toEqual(false)
+      })
+    })
+    describe('editDescription', () => {
+      it('should set edit.description as false', () => {
+        const action = actions.editDescription(task.id)
+        const actual = reducer({ tasks: [task] }, action)
+
+        expect(actual.tasks[0].edit.description).toEqual(true)
+      })
+    })
+  })
+
+  describe('async thunks', () => {
+    afterEach(() => fetchMock.restore())
+
+    describe('index', () => {
+      const item1 = {
+        id: 1,
+        name: 'task1',
+      }
+      const item2 = {
+        id: 2,
+        name: 'task2',
+      }
+      const endpoint = 'http://localhost:3000/tasks.json'
+
+      it('should GET /tasks.json', async () => {
+        const action = index()
+        fetchMock.get(endpoint, {
+          status: 200,
+          body: JSON.stringify([item1, item2]),
+        })
+
+        const subject = await action(jest.fn(), jest.fn(), undefined)
+
+        expect(subject.payload).toEqual([item1, item2])
+        expect(fetchMock).toHaveFetched(endpoint)
+      })
+
+      it('should set pending=true when initiated', () => {
+        const actual = reducer(initialState, index.pending())
+        expect(actual.pending).toBe(true)
+      })
+
+      it('should set pending=false when API call has finished', () => {
+        const actual = reducer(
+          { ...initialState, pending: true },
+          index.fulfilled([])
+        )
+        expect(actual.pending).toBe(false)
+      })
+
+      it('should update the list', () => {
+        const apiRet = [item1, item2]
+        const expected = apiRet.map((i) => ({
+          ...i,
+          edit: { name: false, description: false },
+        }))
+
+        const actual = reducer(
+          {
+            ...initialState,
+            tasks: [
+              {
+                id: 0,
+                name: 'existing task',
+                edit: { name: false, description: false },
+              },
+            ],
+          },
+          index.fulfilled(apiRet)
+        )
+
+        expect(actual.tasks.length).toEqual(expected.length)
+        expect(actual.tasks).toEqual(expected)
+      })
+
+      it('should set pending=false when API call has failed', () => {
+        const actual = reducer(
+          { ...initialState, pending: true },
+          index.rejected()
+        )
+        expect(actual.pending).toBe(false)
+      })
+    })
+
+    describe('create', () => {
+      const newItem = {
+        id: 1,
+        name: 'new task name',
+      }
+      const endpoint = 'http://localhost:3000/tasks.json'
+
+      it('should POST /tasks.json', async () => {
+        const action = create({ name: newItem.name })
+        fetchMock.post(endpoint, {
+          status: 201,
+          body: JSON.stringify(newItem),
+        })
+
+        const subject = await action(jest.fn(), jest.fn(), undefined)
+
+        expect(fetchMock).toHaveFetched(
+          (u, o) =>
+            u == endpoint && o.body == JSON.stringify({ name: newItem.name })
+        )
+      })
+
+      it('should set pending=true when initiated', () => {
+        const actual = reducer(initialState, create.pending())
+        expect(actual.pending).toBe(true)
+      })
+
+      it('should set pending=false when API call has finished', () => {
+        const actual = reducer(
+          { ...initialState, pending: true },
+          create.fulfilled(newItem)
+        )
+        expect(actual.pending).toBe(false)
+      })
+
+      it('should add the new item at beginning of the list', () => {
+        const actual = reducer(
+          { ...initialState, tasks: [{ id: 0, name: 'existing task' }] },
+          create.fulfilled(newItem)
+        )
+
+        expect(actual.tasks.length).toEqual(2)
+        expect(actual.tasks[0]).toEqual({
+          ...newItem,
+          edit: { name: false, description: false },
+        })
+      })
+
+      it('should set pending=false when API call has failed', () => {
+        const actual = reducer(
+          { ...initialState, pending: true },
+          create.rejected()
+        )
+        expect(actual.pending).toBe(false)
+      })
+    })
+
+    describe('update', () => {
+      const item = {
+        id: 1,
+        name: 'existing task name',
+      }
+      const updatedItem = { ...item, description: 'new description' }
+      const endpoint = `http://localhost:3000/tasks/${item.id}.json`
+
+      it('should PUT /tasks/1.json with new values as a payload', async () => {
+        const action = update(updatedItem)
+        fetchMock.put(endpoint, {
+          status: 200,
+          body: JSON.stringify(updatedItem),
+        })
+
+        const subject = await action(jest.fn(), jest.fn(), undefined)
+
+        expect(subject.payload).toEqual(updatedItem)
+        expect(fetchMock).toHaveFetched(
+          (u, o) => u == endpoint && o.body == JSON.stringify(updatedItem)
+        )
+      })
+
+      it('should set pending=true when initiated', () => {
+        const actual = reducer(initialState, update.pending())
+        expect(actual.pending).toBe(true)
+      })
+
+      it('should set pending=false when API call has finished', () => {
+        const actual = reducer(
+          { ...initialState, pending: true },
+          update.fulfilled(updatedItem)
+        )
+        expect(actual.pending).toBe(false)
+      })
+
+      it('should add the new item at beginning of the list', () => {
+        const actual = reducer(
+          { ...initialState, tasks: [item] },
+          update.fulfilled(updatedItem)
+        )
+
+        expect(actual.tasks[0]).toEqual({
+          ...updatedItem,
+          edit: { name: false, description: false },
+        })
+      })
+
+      it('should set pending=false when API call has failed', () => {
+        const actual = reducer(
+          { ...initialState, pending: true },
+          update.rejected()
+        )
+        expect(actual.pending).toBe(false)
+      })
+    })
+
+    describe('destroy', () => {
+      const item = {
+        id: 1,
+        name: 'existing task name',
+      }
+      const endpoint = `http://localhost:3000/tasks/${item.id}.json`
+
+      it('should DELETE /tasks/1.json', async () => {
+        const action = destroy({ id: item.id })
+        fetchMock.delete(endpoint, {
+          status: 204,
+        })
+
+        const subject = await action(jest.fn(), jest.fn(), undefined)
+
+        expect(fetchMock).toHaveFetched(endpoint)
+        expect(subject.payload.id).toEqual(item.id)
+      })
+
+      it('should set pending=true when initiated', () => {
+        const actual = reducer(initialState, destroy.pending())
+        expect(actual.pending).toBe(true)
+      })
+
+      it('should set pending=false when API call has finished', () => {
+        const actual = reducer(
+          { ...initialState, pending: true },
+          destroy.fulfilled({ id: item.id })
+        )
+        expect(actual.pending).toBe(false)
+      })
+
+      it('should add the new item at beginning of the list', () => {
+        const actual = reducer(
+          { ...initialState, tasks: [item, { ...item, ...{ id: 0 } }] },
+          destroy.fulfilled({ id: item.id })
+        )
+
+        expect(actual.tasks.length).toEqual(1)
+        expect(actual.tasks[0]).not.toEqual(item)
+      })
+
+      it('should set pending=false when API call has failed', () => {
+        const actual = reducer(
+          { ...initialState, pending: true },
+          destroy.rejected()
+        )
+        expect(actual.pending).toBe(false)
+      })
+    })
+  })
+})
---
+++ tasks_ui/src/state/tasksSlice.ts
@@ -0,0 +1,129 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+
+const initialState: State = {
+  tasks: [],
+  pending: false,
+}
+
+export const index = createAsyncThunk('task/index', async (params, _) => {
+  const ret = await fetch('http://localhost:3000/tasks.json', {
+    method: 'GET',
+    mode: 'cors',
+    headers: { 'Content-Type': 'application/json' },
+  })
+
+  if (ret.ok) return ret.json()
+})
+
+export const create = createAsyncThunk('task/create', async (params, _) => {
+  const payload = {
+    name: params.name,
+  }
+  const ret = await fetch('http://localhost:3000/tasks.json', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+
+  if (ret.ok) {
+    return ret.json()
+  }
+})
+
+export const update = createAsyncThunk('task/update', async (params, _) => {
+  const ret = await fetch(`http://localhost:3000/tasks/${params.id}.json`, {
+    method: 'PUT',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(params),
+  })
+
+  if (ret.ok) return ret.json()
+})
+
+export const destroy = createAsyncThunk('task/destroy', async (params, _) => {
+  const ret = await fetch(`http://localhost:3000/tasks/${params.id}.json`, {
+    method: 'DELETE',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  if (ret.ok) return { id: params.id }
+})
+
+const findTask = (state, id) => {
+  return state.tasks.find((t) => id == t.id)
+}
+
+const edit = { name: false, description: false }
+
+export const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    showName(state, action) {
+      const target = findTask(state, action.payload)
+      target.edit.name = false
+    },
+    editName(state, action) {
+      const target = findTask(state, action.payload)
+      target.edit.name = true
+    },
+    showDescription(state, action) {
+      const target = findTask(state, action.payload)
+      target.edit.description = false
+    },
+    editDescription(state, action) {
+      const target = findTask(state, action.payload)
+      target.edit.description = true
+    },
+  },
+
+  extraReducers: (builder) => {
+    ;[index, create, update, destroy].forEach((t) => {
+      builder.addCase(t.pending, (s) => {
+        s.pending = true
+      })
+      builder.addCase(t.rejected, (s) => {
+        s.pending = false
+      })
+    })
+
+    builder.addCase(index.fulfilled, (state, action) => {
+      state.pending = false
+      state.tasks = action.payload.map((i) => ({
+        ...i,
+        edit,
+      }))
+    })
+
+    builder.addCase(create.fulfilled, (state, action) => {
+      state.pending = false
+
+      state.tasks = [{ ...action.payload, edit }, ...state.tasks]
+    })
+
+    builder.addCase(update.fulfilled, (state, action) => {
+      state.pending = false
+
+      const index = state.tasks.findIndex((t) => t.id == action.payload.id)
+      state.tasks[index] = {
+        ...action.payload,
+        edit,
+      }
+    })
+
+    builder.addCase(destroy.fulfilled, (state, action) => {
+      state.pending = false
+
+      state.tasks = state.tasks.filter((t) => t.id != action.payload.id)
+    })
+  },
+})
